57e4c0dcd8cbe95ff9c539256cbf63b16b213cba
muhtar4506 <148867570+muhtar4506@users.noreply.github.com>
2024-04-06 16:11:08 +0300

    init commit
    init commit

---
+++ audi-rv-master/app/misc/color.js
@@ -0,0 +1,9 @@
+export default {
+  APP_BG: '#ac6b6b',
+  FONT: '#303d49',
+  FONT_MEDIUM: '#636363',
+  FONT_LIGHT: '#b6b8b9',
+  MODAL_BG: 'rgba(0, 0, 0, 0.2)',
+  ACTIVE_BG: '#000000',
+  ACTIVE_FONT: '#ac6b6b',
+};
